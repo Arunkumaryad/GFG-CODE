617a10f35fa5361fcac4ce6b655d93a4d596ce3e
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-05-01 01:23:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Encrypt the string - 1/encrypt-the-string-1.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string encryptString(string s){
+    // code here
+    string ans = "";
+    int cnt = 1;
+    for(int i=0;i<s.size();i++) {
+        if(s[i] == s[i+1]) {
+            cnt++;
+        }
+        else {
+            ans += s[i];
+            ans += to_string(cnt);
+            cnt = 1;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+		cin>>s;
+		Solution ob;
+		string ans = ob.encryptString(s);
+		cout<<ans<<endl;
+	}
+}
+
+// } Driver Code Ends
