26cf8f53c66a4846a1f72102309a53dc674eba91
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-05-05 01:42:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find duplicates in an array/find-duplicates-in-an-array.cpp
+++ Easy/Find duplicates in an array/find-duplicates-in-an-array.cpp
@@ -7,8 +7,8 @@ class Solution{
   public:
     vector<int> duplicates(long long arr[], int n) {
         // code here
-        unordered_map<int,int>mp;
         vector<int>ans;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++) {
             mp[arr[i]]++;
         }
