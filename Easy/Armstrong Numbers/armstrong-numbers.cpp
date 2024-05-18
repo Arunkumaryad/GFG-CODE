d9307680c1189208cc833fa0651d0e33ea4acb9c
Arun kumar Yadav <124424629+Arunkumaryad@users.noreply.github.com>
2024-05-18 18:07:49 +0530

    Updated Solution - GfG to GitHub

---
+++ Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        // code here
+        int sum = 0;
+        int num = n;
+        while(num) {
+            int digit = num % 10;
+            sum += (digit*digit*digit);
+            num /= 10;
+        }
+        if(sum == n) {
+            return "Yes";
+        }
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
